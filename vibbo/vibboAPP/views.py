8c7e7ad54743c42ae663cfbc9006991adb6d7e58
marc <mymberca7.@alumnes.ub.edu>
2019-10-02 17:48:37 +0200

    Main functionalities working
    Sign Up, Log In, Log Out, Starting Page working

--- vibbo/vibboAPP/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello. You're at the VIBBO home page.")
-# Create your views here.
