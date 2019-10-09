c8daa4a5549c26fec432278ba8f0a2cb7135722e
Lukas Kubacki <lukas@kubacki.cz>
2019-10-09 11:43:19 +0200

    Add basic profile model

--- vibbo/vibboAPP/models.py
+++ vibbo/vibboAPP/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    biography = models.TextField(max_length=500, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
