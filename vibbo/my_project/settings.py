4a74dd9392edfab78fbfd2a845ec63911f1841a7
marc <mymberca7.@alumnes.ub.edu>
2019-12-17 21:21:24 +0100

    heroku test

--- vibbo/my_project/settings.py
+++ vibbo/my_project/settings.py
@@ -74,12 +74,8 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
 
 
 # Password validation
